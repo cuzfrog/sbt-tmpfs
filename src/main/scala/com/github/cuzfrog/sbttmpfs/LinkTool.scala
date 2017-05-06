4af06e1199db6c7101a30e73956df95314bdb70b
Cause Chung <cuzfrog@139.com>
2017-05-07 02:05:22 +0800

    Add clean logic.

--- src/main/scala/com/github/cuzfrog/sbttmpfs/LinkTool.scala
+++ src/main/scala/com/github/cuzfrog/sbttmpfs/LinkTool.scala
@@ -1,13 +1,32 @@
 package com.github.cuzfrog.sbttmpfs
 
 import java.io.File
-import java.nio.file.{Files, Paths}
+import java.nio.file.Paths
 
-import scala.collection.concurrent.TrieMap
 import sbt._
 
+import scala.collection.concurrent.TrieMap
+
 private object LinkTool {
-  def link(targetDirs: Seq[File], baseTmpfsDirectory: File)(implicit logger: Logger): Unit =
+
+  //symlinks and their target tmpfs dir, used for clean old links after task clean.
+  private val linkedDirsRecord = TrieMap.empty[File, File]
+
+  /**
+    * Try to symbolic-link target directories into base tmpfs directory.
+    * <br><br>
+    * If target directory is an active symlink or already of tmpfs, abort.
+    * If base tmpfs directory is actually not of tmpfs, abort and warn.
+    * <br><br>
+    * If target directory already exists, move its contents(if it has)
+    * to newly created dir inside base tmpfs dir. And then create the link.
+    * If target is a dead link, delete it first.
+    *
+    * @param targetDirs         seq of target dirs to be linked.
+    * @param baseTmpfsDirectory a base dir that should be of tmpfs.
+    * @param logger             sbt logger.
+    */
+  def link(targetDirs: Seq[File], baseTmpfsDirectory: File)(implicit logger: Logger): Unit = this.synchronized {
     targetDirs.foreach { targetDir =>
       if (targetDir.isActiveLink || targetDir.isOfTmpfs) {
         logger.debug("[SbtTmpfsPlugin] targetDir is already an active symlink or of tmpfs.")
@@ -42,18 +61,33 @@ private object LinkTool {
       logger.debug("[SbtTmpfsPlugin] execute shell command:" + cmd)
       val output = Process(cmd) !!
 
-      if (output.isDefined) logger.error(s"[SbtTmpfsPlugin] tmpfs link failed with info: $output")
+      if (output.isDefined) {
+        logger.error(s"[SbtTmpfsPlugin] tmpfs link failed with info: $output")
+      } else {
+        //if linking is successful, record symlink and its linked-target tmpfs dir.
+        //And delete the previous linked-target tmpfs dir.
+        linkedDirsRecord.put(targetDir, tmpfsDir).foreach(IO.delete)
+      }
     }
-
-
+  }
+  /**
+    * Try to mount target directory with tmpfs.
+    * <br><br>
+    * If target is not directory or does not exist, abort.
+    * If target is already of tmpfs, abort.
+    *
+    * @param targetDir the target directory is about to mount to.
+    * @param mountCmd  the shell mount command string.
+    * @param logger    sbt logger.
+    */
   def mount(targetDir: File, mountCmd: String)(implicit logger: Logger): Unit = {
-    if(!targetDir.isDirectory){
+    if (!targetDir.isDirectory) {
       logger.warn(s"[SbtTmpfsPlugin] targetDir is not a directory," +
         s" abort mounting tmpfs. Path:${targetDir.getCanonicalPath}")
       return
     }
 
-    if(targetDir.isOfTmpfs){
+    if (targetDir.isOfTmpfs) {
       logger.debug("[SbtTmpfsPlugin] targetDir is already of tmpfs, abort mounting.")
       return
     }
@@ -62,6 +96,7 @@ private object LinkTool {
     if (output.isDefined) logger.error(s"[SbtTmpfsPlugin] tmpfs mount failed with info: $output")
   }
 
+  // -------------------------- Helpers --------------------------
   private implicit class ExFile(f: File)(implicit logger: Logger) {
     def isLink: Boolean = {
       if (!f.exists) {
